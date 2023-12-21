0183cef6435608fcf7c09042206ab1e751fc5923
Andrew DGalga <ex.spake@gmail.com>
2023-12-21 17:50:40 -0600

    filesystem reading as easy as expected so far.

--- main.gd
+++ main.gd
@@ -1,1 +1,10 @@
 extends Control
+
+var lorem = ""
+
+func _ready():
+	var file = FileAccess.open("res://sourcetext.txt", FileAccess.READ)
+	lorem = file.get_as_text()
+	
+	if lorem != "":
+		print(lorem)
