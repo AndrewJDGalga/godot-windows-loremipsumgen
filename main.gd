db6581f2e5a48b594823ce47d0d3b583a479a265
Andrew DGalga <ex.spake@gmail.com>
2023-12-21 18:26:25 -0600

    paragraph count alignment tweek, copybutton signal to main scene script.

--- main.gd
+++ main.gd
@@ -1,13 +1,21 @@
 extends Control
 
-var clipboardObj = null
+var copyBtn = null
+var paragraphCount = null
 var lorem = ""
 
 func _ready():
 	var file = FileAccess.open("res://sourcetext.txt", FileAccess.READ)
 	lorem = file.get_as_text()
-	var testText = lorem + "\n" + lorem
-	copyToClipboard(testText)
+	copyBtn = $copy_button
+	paragraphCount = $paragraph_count
 
 func copyToClipboard(text):
 	DisplayServer.clipboard_set(text)
+
+func onCopyPress():
+	pass
+
+
+func _on_copy_button_pressed():
+	pass # Replace with function body.
