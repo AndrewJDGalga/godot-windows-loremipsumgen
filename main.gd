8ed09903749d4ddef945f9b279ac80dd7380f033
Andrew DGalga <ex.spake@gmail.com>
2023-12-21 18:41:47 -0600

    version 1 done, additional versions would just need proper design and coloring, ignoring that for now.

--- main.gd
+++ main.gd
@@ -1,17 +1,16 @@
 extends Control
 
 var copyBtn = null
-var paragraphCount = null
 var lorem = ""
 
 func _ready():
 	var file = FileAccess.open("res://sourcetext.txt", FileAccess.READ)
 	lorem = file.get_as_text()
 	copyBtn = $copy_button
-	paragraphCount = $paragraph_count
+	copyToClipboard(lorem)
 
 func copyToClipboard(text):
 	DisplayServer.clipboard_set(text)
 
 func _on_copy_button_pressed():
-	pass # Replace with function body.
+	copyToClipboard(lorem)
